32daf4db5c64ddd2aa19489509abe2432a173654
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-09-22 21:26:17 -0300

    init repo

---
+++ App.java
@@ -0,0 +1,26 @@
+
+/**
+ * Escreva a descrição da classe App aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class App
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+
+    /**
+     * COnstrutor para objetos da classe App
+     */
+    public App()
+    {
+        
+    }
+    
+    public static void main(String[] args){
+        
+        System.out.println("This is a test");
+        
+    }
+    
+}
